09d4d3db5b07573346354a2d03b511b72fb2553a
Joao Soares <jgsoares.francisco@gmail.com>
2024-01-12 00:39:56 -0300

    feat: add allowed host logic

--- router/router.go
+++ router/router.go
@@ -1,12 +1,42 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Initialize() {
 	// gin.SetMode(gin.ReleaseMode) TO-DO Active if env == PROD
 	r := gin.Default()
 
+	r.Use(getAllowedSubdomains())
+
 	initializeRoutes(r)
 
 	r.Run(":8080")
+}
+
+func getAllowedSubdomains() gin.HandlerFunc {
+	allowedSubdomains := []string{"admin", "lolis", "www"}
+
+	return func(c *gin.Context) {
+		host := c.Request.Host
+		validSubdomain := false
+		for _, subdomain := range allowedSubdomains {
+			if strings.HasPrefix(host, subdomain+".") {
+				validSubdomain = true
+				break
+			}
+		}
+
+		if !validSubdomain {
+			c.JSON(403, gin.H{
+				"error": "Forbidden",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
